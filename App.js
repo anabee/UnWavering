7c43b095065a3f0d701889d0701c969a38ffad3f
Ana Valdivia <anav1317@yahoo.com>
2020-07-15 16:05:23 -0700

    Added img and bttn for home page

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground source={require('./assets/main.png')} style={styles.image}>
+      <Text style={styles.text}>UnWavering</Text>
+      <Button 
+      title="Login"
+      color="#EEB5B4"
+      onPress={() => Alert.alert('Right button pressed')}
+      ></Button>
+      </ImageBackground>
     </View>
   );
 }
@@ -14,8 +20,13 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  text: {
+    color: `#EEB5B4`,
+    fontSize: 64,
+  },
+  image: {
+    flex: 1,
+    resizeMode: "cover",
   },
 });

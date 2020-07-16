dba5d5c57c034020ae28f973e6d0cbfbf478acb8
Ana Valdivia <anav1317@yahoo.com>
2020-07-15 20:45:42 -0700

    Finally got the heading to center

--- App.js
+++ App.js
@@ -1,17 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground, Button } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <ImageBackground source={require('./assets/main.png')} style={styles.image}>
       <Text style={styles.text}>UnWavering</Text>
-      <Button 
-      title="Login"
-      color="#EEB5B4"
-      onPress={() => Alert.alert('Right button pressed')}
-      ></Button>
+      <TouchableHighlight style={styles.button}>
+        <View>
+          <Text>Login</Text>
+        </View>
+      </TouchableHighlight>
       </ImageBackground>
     </View>
   );
@@ -20,13 +20,24 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-  },
-  text: {
-    color: `#EEB5B4`,
-    fontSize: 64,
+    backgroundColor: '#000',
+    paddingTop: Platform.OS === "ios" ? 100 :0,
+    justifyContent: 'center',
   },
   image: {
     flex: 1,
     resizeMode: "cover",
   },
+  text: {
+    color: `#EEB5B4`,
+    fontSize: 64,
+    textAlign: 'center'
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#EEB5B4",
+    padding: 10,
+    borderRadius: 25,
+    width: "50%",
+  }
 });
